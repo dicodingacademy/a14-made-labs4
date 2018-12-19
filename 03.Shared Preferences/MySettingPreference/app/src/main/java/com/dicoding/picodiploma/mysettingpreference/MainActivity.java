c2b56f620e724fcbd16c834158fab7e387c60b8e
IanLaFlair <luzifer838@gmail.com>
2018-12-19 10:49:09 +0700

    preference.xml

--- 03.Shared Preferences/MySettingPreference/app/src/main/java/com/dicoding/picodiploma/mysettingpreference/MainActivity.java
+++ 03.Shared Preferences/MySettingPreference/app/src/main/java/com/dicoding/picodiploma/mysettingpreference/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction().add(R.id.setting_holder, new MyPreferenceFragment2()).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.setting_holder, new MyPreferenceFragment()).commit();
     }
 }
