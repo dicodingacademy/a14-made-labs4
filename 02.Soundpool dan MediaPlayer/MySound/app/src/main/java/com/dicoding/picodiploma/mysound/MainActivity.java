b6d88e61e76a3b64dd8d325cc794d43798fd8668
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2018-12-26 14:20:08 +0700

    Remove unused resources

--- 02.Soundpool dan MediaPlayer/MySound/app/src/main/java/com/dicoding/picodiploma/mysound/MainActivity.java
+++ 02.Soundpool dan MediaPlayer/MySound/app/src/main/java/com/dicoding/picodiploma/mysound/MainActivity.java
@@ -1,9 +1,7 @@
 package com.dicoding.picodiploma.mysound;
 
-import android.content.Intent;
 import android.media.SoundPool;
 import android.os.Bundle;
-import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
