784797c38e5c5d0372fad5ea364c79cdd3c9d3cb
IanLaFlair <luzifer838@gmail.com>
2018-12-30 15:29:25 +0700

    Refactor & Change To RecyclerView

--- 06.ContentProvider/DicodingNotesApp/app/src/main/java/com/dicoding/picodiploma/dicodingnotesapp/adapter/DicodingNotesAdapter.java
+++ 06.ContentProvider/DicodingNotesApp/app/src/main/java/com/dicoding/picodiploma/dicodingnotesapp/adapter/DicodingNotesAdapter.java
@@ -1,15 +1,26 @@
 package com.dicoding.picodiploma.dicodingnotesapp.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
+import com.dicoding.picodiploma.dicodingnotesapp.CustomOnItemClickListener;
+import com.dicoding.picodiploma.dicodingnotesapp.FormActivity;
 import com.dicoding.picodiploma.dicodingnotesapp.R;
+import com.dicoding.picodiploma.dicodingnotesapp.entity.NoteItem;
 
+import java.util.ArrayList;
+
+import static com.dicoding.picodiploma.dicodingnotesapp.db.DatabaseContract.NoteColumns.CONTENT_URI;
 import static com.dicoding.picodiploma.dicodingnotesapp.db.DatabaseContract.NoteColumns.DATE;
 import static com.dicoding.picodiploma.dicodingnotesapp.db.DatabaseContract.NoteColumns.DESCRIPTION;
 import static com.dicoding.picodiploma.dicodingnotesapp.db.DatabaseContract.NoteColumns.TITLE;
@@ -19,34 +30,68 @@ import static com.dicoding.picodiploma.dicodingnotesapp.db.DatabaseContract.getC
  * Created by dicoding on 12/13/2016.
  */
 
-public class DicodingNotesAdapter extends CursorAdapter {
+public class DicodingNotesAdapter extends RecyclerView.Adapter<DicodingNotesAdapter.NoteViewHolder> {
 
-    public DicodingNotesAdapter(Context context, Cursor c, boolean autoRequery) {
-        super(context, c, autoRequery);
+    private final ArrayList<NoteItem> listNotes = new ArrayList<>();
+    private final Activity activity;
+
+    public DicodingNotesAdapter(Activity activity) {
+        this.activity = activity;
     }
 
+    public ArrayList<NoteItem> getListNotes() {
+        return listNotes;
+    }
 
+    public void setListNotes(ArrayList<NoteItem> listNotes) {
+        if (listNotes.size() > 0){
+            this.listNotes.clear();
+        }
+        this.listNotes.addAll(listNotes);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-        return LayoutInflater.from(context).inflate(R.layout.item_dicoding_note, viewGroup, false);
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dicoding_note, parent, false);
+        return new NoteViewHolder(view);
     }
 
     @Override
-    public Cursor getCursor() {
-        return super.getCursor();
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int i) {
+        holder.tvTitle.setText(getListNotes().get(i).getTitle());
+        holder.tvDate.setText(getListNotes().get(i).getDate());
+        holder.tvDescription.setText(getListNotes().get(i).getDescription());
+        holder.itemView.setOnClickListener(new CustomOnItemClickListener(i, new CustomOnItemClickListener.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(View view, int position) {
+                Intent intent = new Intent(activity, FormActivity.class);
+
+                // Set intent dengan data uri row note by id
+                // content://com.dicoding.picodiploma.mynotesapp/note/id
+                Uri uri = Uri.parse(CONTENT_URI + "/" + getListNotes().get(position).getId());
+                intent.setData(uri);
+                activity.startActivity(intent);
+            }
+        }));
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        if (cursor != null){
-            TextView tvTitle = view.findViewById(R.id.tv_item_title);
-            TextView tvDate = view.findViewById(R.id.tv_item_date);
-            TextView tvDescription = view.findViewById(R.id.tv_item_description);
+    public int getItemCount() {
+        return listNotes.size();
+    }
 
+    public class NoteViewHolder extends RecyclerView.ViewHolder {
+        final TextView tvTitle, tvDescription, tvDate;
+        View rootView;
 
-            tvTitle.setText(getColumnString(cursor,TITLE));
-            tvDescription.setText(getColumnString(cursor,DESCRIPTION));
-            tvDate.setText(getColumnString(cursor,DATE));
+        public NoteViewHolder(View itemView) {
+            super(itemView);
+            rootView = itemView;
+            tvTitle = itemView.findViewById(R.id.tv_item_title);
+            tvDescription = itemView.findViewById(R.id.tv_item_description);
+            tvDate = itemView.findViewById(R.id.tv_item_date);
         }
     }
 }
