2b65c84f714718555ac66f765d60abdaf91be9af
Ahmad Arif Faizin <faizin.arief@gmail.com>
2021-09-02 11:32:26 +0700

    update gradle

--- Read dan WriteFile/MyReadWriteFile/java/app/src/main/java/com/dicoding/picodiploma/myreadwritefile/MainActivity.java
+++ Read dan WriteFile/MyReadWriteFile/java/app/src/main/java/com/dicoding/picodiploma/myreadwritefile/MainActivity.java
@@ -1,13 +1,13 @@
 package com.dicoding.picodiploma.myreadwritefile;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Collections;
